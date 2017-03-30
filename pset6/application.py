a4b4466ebc567f7370b9aed96c6616ab83bba1a6
JJ <jj.spetseris@yahoo.com>
2017-03-30 12:21:21 -0500

    pset6 final

---
+++ pset6/application.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+import helpers
+import os
+import sys
+from analyzer import Analyzer
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+
+    # validate screen_name
+    screen_name = request.args.get("screen_name", "").lstrip("@")
+    if not screen_name:
+        return redirect(url_for("index"))
+
+    # get screen_name's tweets
+    tweets = helpers.get_user_timeline(screen_name)
+
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+
+    # instantiate analyzer
+    analyzer = Analyzer(positives, negatives)
+
+    # initializes variables for next step
+    score = 0
+    positive, negative, neutral = 0.0, 0.0, 0.0
+
+    # calculates how many positive, negative and neutral tweets there are
+    x = len(tweets)
+    for tweet in range(x):
+        score = analyzer.analyze(tweets[tweet])
+        if score > 0.0:
+            positive += 1
+        elif score < 0.0:
+            negative += 1
+        else:
+            neutral += 1
+
+
+    # generate chart
+    chart = helpers.chart(positive, negative, neutral)
+
+    # render results
+    return render_template("search.html", chart=chart, screen_name=screen_name)
---
+++ pset6/tweets/analyzer.py
@@ -0,0 +1,37 @@
+import nltk
+
+class Analyzer():
+    """Implements sentiment analysis."""
+
+    def __init__(self, positives, negatives):
+        """Initialize Analyzer."""
+        pos_words = []
+        neg_words = []
+
+        File_pos = open(positives, "r")
+        for lines in File_pos:
+            if not lines.startswith(";") and not lines.startswith("\n"):
+                pos_words.append(lines.strip("\n"))
+        self.positives = pos_words
+
+        File_neg = open(negatives, "r")
+        for lines in File_neg:
+            if not lines.startswith(";") and not lines.startswith("\n"):
+                neg_words.append(lines.strip("\n"))
+        self.negatives = neg_words
+
+    def analyze(self, text):
+        """Analyze text for sentiment, returning its score."""
+        score = 0
+
+        tokenizer = nltk.tokenize.TweetTokenizer()
+
+        tokens = tokenizer.tokenize(text)
+
+        for token in tokens:
+            if token.lower() in self.positives:
+                score +=1
+            if token.lower() in self.negatives:
+                score -= 1
+
+        return score
