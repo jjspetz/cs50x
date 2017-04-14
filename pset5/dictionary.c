257b68fc5228e2f4572a1a735e403dcfc1d257e5
JJ <jj.spetseris@yahoo.com>
2017-04-14 10:07:32 -0500

    working version

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "dictionary.h"
 
@@ -20,7 +21,7 @@ typedef struct node
 }
 node;
 
-// hash table
+// hash table declaration
 node* hashtable[MAX];
 
 // dictionary word counter
@@ -29,7 +30,7 @@ int words;
 /**
  * Returns true if word is in dictionary else false.
  */
-bool check(char *word)
+bool check(const char *word)
 {
     /***********
      *
@@ -38,22 +39,35 @@ bool check(char *word)
      * 3. if found return true else return false
      *
      ************/
-    // hash word
-    int index = hash(word);
 
-    // declare a node pointer variable to "look through the list"
+    // declare useful variables
+    int word_len = strlen(word);
+    char checkword[word_len+1];
+
+    // changes word to lower case and writes it into checkword variable
+    for (int i=0; i< word_len; ++i)
+        checkword[i] = tolower(word[i]);
+
+    // adds word break to end of checkword
+    checkword[word_len] = '\0';
+
+    // looks up place in hashtable
     node* look;
-    look = hashtable[index];
+    look = hashtable[hash(checkword)];
 
-    // loops through the linked list looking for a match
-    while (look->next != NULL)
+    // loops through linked list if any
+    while(look)
     {
-        if (look->word == word)
+        // strcmp compares a string and returns 0 if same
+        if (strcmp(look->word, checkword) == 0)
+        {
             return true;
+        }
+        // continues through linked list if first value is not the same
         else
             look = look->next;
     }
-
+    // returns false to indicate the word was not found in the dictionary
     return false;
 }
 
@@ -74,12 +88,17 @@ bool load(const char *dictionary)
     // creates buffer of maximum length for fgets
     char buffer[LENGTH + 1];
 
+    // hash table initilization
+    for (int i = 0; i < MAX; i++)
+    hashtable[i] = NULL;
+
+
     // loops through the file one word at a time
     while (fscanf(dict, "%s", buffer) != EOF){
 
         // create new node
-        node *n = malloc(sizeof(*n));
-        if (n ==NULL)
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
         {
             printf("out of memory");
             return false;
@@ -87,6 +106,7 @@ bool load(const char *dictionary)
 
         // copies buffer into the new node
         strcpy(n->word, buffer);
+        n->next = NULL;
 
         // checks to see if hashtable placement is empty
         if (hashtable[hash(buffer)]==NULL)
@@ -128,26 +148,44 @@ unsigned int size(void)
  */
 bool unload(void)
 {
-    // TODO
-    return false;
+    // declare a node pointer variable to "look through the list" and to clear list as it goes
+    node* look;
+    node* clear;
+
+    // loops through the hashtable
+    for (int i = 0; i < MAX; i++)
+    {
+        // looks at current hashtable index
+        look = hashtable[i];
+
+        // loops through the linked list freeing as it goes
+        while (look)
+        {
+            clear = look;
+            look = look->next;
+            free(clear);
+        }
+    }
+
+    return true;
 }
 
 	/**************************
 	* Hash Function
 	*
-	* I will use the sdbm hash function.
+	* I will use the djb2 hash function.
 	* code from https://github.com/batanete/CHashTables/blob/master/hashtables.c
 	* It is modified for use in my program.
 	*
 	**************************/
-	unsigned int hash(char* key){
-	//unsigned long res = 5381; //djb2
-	unsigned int res = 0; // sdbm
+	unsigned int hash(const char* key){
+	unsigned int res = 5381; //djb2
+	//unsigned int res = 0; // sdbm
     int c;
 
     while ((c = *key++))
-		//res = ((res << 5) + res) + c; /* hash * 33 + c (djb2)*/
-		res = c + (res << 6) + (res << 16) - res; //sdbm
+		res = ((res << 5) + res) + c; /* hash * 33 + c (djb2)*/
+		//res = c + (res << 6) + (res << 16) - res; //sdbm
 
 	// makes sure hash value will fit into my hash table
 	res = res % MAX;
